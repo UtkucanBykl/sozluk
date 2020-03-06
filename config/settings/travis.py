0e11b65359a1c922838887c5f32a7cc437778092
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2020-03-06 11:41:06 +0300

    We are no more uses to sqlite for test. I added postgresql settings for travis

--- config/settings/travis.py
+++ config/settings/travis.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'x'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'travisci',
+        'NAME': 'mydb',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
