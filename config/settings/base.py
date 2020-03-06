60847a39f22b8acb6a51b5f370b9a17fa307945a
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2020-03-06 10:27:32 +0300

    Added Smartbox class for operetion of smartbox

--- config/settings/base.py
+++ config/settings/base.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static_root/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
