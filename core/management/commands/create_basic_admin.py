48c467c22e306be7808aa85c9dbf5e99f20cbb67
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2020-03-10 10:15:07 +0300

    Added new command for create admin with test fields. I should add this command because i couldnt login admin page when i use the review up.

---
+++ core/management/commands/create_basic_admin.py
@@ -0,0 +1,20 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.management.commands import createsuperuser
+from django.core.management import CommandError
+
+
+User = get_user_model()
+
+
+class Command(createsuperuser.Command):
+    help = 'Crate a superuser, and allow password to be provided'
+
+    def handle(self, *args, **options):
+        password = os.environ.get('BASIC_USER_PASS')
+        user = User.objects.create_superuser(
+            username='testt', email='testt@test.com', password=password, first_name='utku', last_name='can'
+            )
+        user.save()
+        print('Superuser created')
