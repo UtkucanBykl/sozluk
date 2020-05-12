d318ad41612ba0aed0c0908cd2c70f5b48380f91
utkucanbykl <utkucanbykl@gmail.com>
2020-05-13 00:19:14 +0300

    I should control entry of user is deleted

--- core/filters/title.py
+++ core/filters/title.py
@@ -9,8 +9,11 @@ __all__ = ['TitleFilter']
 class TitleFilter(rest_framework.FilterSet):
     category = rest_framework.CharFilter(field_name='category__name', lookup_expr='iexact')
     title = rest_framework.CharFilter(field_name='title', lookup_expr='iexact')
-    entry_username = rest_framework.CharFilter(field_name='entry__user__username', lookup_expr='iexact', distinct=True)
+    entry_username = rest_framework.CharFilter(method='filter_by_username')
 
     class Meta:
         model = Title
         fields = ('category', 'title')
+
+    def filter_by_username(self, queryset, name, value):
+        return queryset.filter(entry__user__username=value, entry__status='publish').distinct()
