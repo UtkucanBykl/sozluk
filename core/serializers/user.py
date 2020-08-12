84966a833ff23e0afdbeddd40ec282fdc7f5b7c8
utkucanbykl <utkucanbykl@gmail.com>
2020-08-12 19:36:16 +0300

    I've added gropus field to UserSerializer

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
@@ -7,12 +8,20 @@ __all__ = ['UserSerializer']
 User = get_user_model()
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'id')
+        model = Group
+
+
 class UserSerializer(serializers.ModelSerializer):
     token = serializers.SerializerMethodField()
+    groups = GroupSerializer(read_only=True, many=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'is_active', 'is_superuser', 'is_staff', 'first_name', 'last_name', 'token')
+        fields = ('username', 'email', 'is_active', 'is_superuser', 'is_staff', 'first_name', 'last_name', 'token',
+                  'groups')
 
     def get_token(self, obj):
         return Token.objects.filter(user=obj).first().key
