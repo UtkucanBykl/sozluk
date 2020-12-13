3a03aa4649a53ebbf296e124fafd2b0693181191
utkucan.biyikli <utkucanbykl@gmail.com>
2020-12-13 18:00:36 +0300

    Added query param

--- core/pagination/pagination.py
+++ core/pagination/pagination.py
@@ -5,15 +5,19 @@ __all__ = ['StandardPagination', 'LargePagination', "StandardTitlePagination", "
 
 class StandardPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = "page_size"
 
 
 class LargePagination(PageNumberPagination):
     page_size = 100
+    page_size_query_param = "page_size"
 
 
 class StandardTitlePagination(PageNumberPagination):
     page_size = 33
+    page_size_query_param = "page_size"
 
 
 class StandardEntryPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = "page_size"
