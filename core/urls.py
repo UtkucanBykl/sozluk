84194404023242af9ac2bb2f198224e4746bbf4c
tugay <tugay.celmeli.7@gmail.com>
2021-01-04 21:39:32 +0300

    blocks list added

--- core/urls.py
+++ core/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("titles/blocks/<int:title_id>/", NotShowTitleCreateAPIView.as_view({"delete": "destroy"}), name="block-delete"),
     path("users/password/change/", ChangeUserPasswordView.as_view(), name="change-password"),
     path("blocks/",
-         BlockUserViewSet.as_view({"post": "create"}),
+         BlockUserViewSet.as_view({"post": "create", "get": "list"}),
          name="user-block-create"),
     path("blocks/<int:blocked_user_id>/",
          BlockUserViewSet.as_view({"patch": "partial_update", "delete": "destroy"}),
