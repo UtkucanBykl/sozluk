883da147ad595dd4d581f60872031a94e9f2d54d
tugayclml <tugay.celmeli.7@gmail.com>
2021-08-27 21:38:40 +0300

    UserFollow-Entry-Notification-User

--- core/models/notification.py
+++ core/models/notification.py
@@ -15,15 +15,19 @@ class Notification(BaseModel):
     info = 'info'
     favorite = 'favorite'
     username_title = 'username_title'
+    dislike = 'dislike'
+    info_title_entries = 'info_title_entries'
     notification_types = (
         (follow, follow),
         (like, like),
         (mention, mention),
         (info, info),
         (favorite, favorite),
-        (username_title, username_title)
+        (username_title, username_title),
+        (dislike, dislike),
+        (info_title_entries, info_title_entries)
     )
-    notification_type = models.CharField(max_length=16, choices=notification_types, default='info')
+    notification_type = models.CharField(max_length=25, choices=notification_types, default='info')
     receiver_user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
     sender_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     entry = models.ForeignKey(Entry, blank=True, null=True, on_delete=models.CASCADE)
@@ -31,6 +35,8 @@ class Notification(BaseModel):
     message = models.CharField(max_length=140)
     redirect = models.URLField(blank=True, null=True, max_length=500)
     is_open = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    is_seen = models.BooleanField(default=False)
 
     def __str__(self):
         return self.message
