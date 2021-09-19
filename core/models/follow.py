d6a43fe4dd59017f71e681605c72bd458009923f
tugayclml <tugay.celmeli.7@gmail.com>
2021-09-19 18:28:08 +0300

    user follow return pk

--- core/models/follow.py
+++ core/models/follow.py
@@ -21,4 +21,4 @@ class UserFollow(BaseModel):
     following_user = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.user.username} '
+        return f'{self.pk} '
